c9975f546f1f0dddf33f80df0b7bb66f7706e7b7
Kristiyan Iliev <kristiyaniliev@Kristiyans-MacBook-Pro.local>
2025-04-01 18:57:13 +0100

    Initial commit

---
+++ website/eslint.config.mjs
@@ -0,0 +1,22 @@
+import {defineConfig} from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+    eslintConfigPrettier,
+    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: {globals: globals.browser},
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        plugins: {js},
+        extends: ["js/recommended"],
+    },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+]);
